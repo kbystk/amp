0d3a91ebda33aaf1dbf1ccc53a916c43e852ea64
jigsaw <m@jgs.me>
2019-08-16 17:17:50 +0900

    Update

--- sitemap.js
+++ sitemap.js
@@ -1,14 +1,14 @@
 const { writeFileSync } = require('fs')
 const crypto = require('crypto')
 const { createSitemap } = require('sitemap')
-const data = require('./data/html.json')
+const data = require('./data/pub.json')
 
 const sitemap = createSitemap({
   hostname: 'https://amp.kbys.tk',
   cacheTime: 600000,
-  urls: data.map(article => {
+  urls: data.pages.map(page => {
     let sha = crypto.createHash('sha1')
-    sha.update(article.title)
+    sha.update(page.title)
     const hash = sha.digest('hex')
     return {
       url: `/${hash}.html`
