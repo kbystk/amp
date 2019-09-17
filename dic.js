16ec3ff34fabb589d94646cce734540549413aff
jigsaw <m@jgs.me>
2019-09-17 11:09:31 +0900

    Commonize

--- dic.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require('fs')
-const master = require('./data/pub.json')
-
-let dic = {}
-master.forEach(article => {
-  dic[article.title] = article.id
-})
-
-writeFileSync('./data/dic.json', JSON.stringify(dic))
