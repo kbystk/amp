e6bdadef23dbc7de58655eeae33cbef44b600e82
jigsaw <m@jgs.me>
2019-10-30 18:00:01 +0900

    Send views to pixe.la

--- views.js
+++ views.js
@@ -1,6 +1,28 @@
-const { writeFileSync } = require('fs')
+const { writeFileSync, readFileSync } = require('fs')
+const fetch = require('isomorphic-unfetch')
+const format = require('date-fns/format')
+const subDays = require('date-fns/subDays')
 const master = require('./data/pub.json')
 
-const count = master.reduce((prev, curr) => (prev += curr.views), 0)
-console.log(`Total views: ${count}`)
-writeFileSync('./public/COUNT', count)
+const PATH = './public/COUNT'
+
+const main = async () => {
+  const prevCount = parseInt(readFileSync(PATH).toString())
+  const count = master.reduce((prev, curr) => (prev += curr.views), 0)
+  const diff = count - prevCount
+  await fetch('https://pixe.la/v1/users/jgs/graphs/sbv', {
+    headers: {
+      'X-USER-TOKEN': process.env.PIXE_LA_TOKEN
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      date: format(subDays(new Date(), 1), 'yyyyMMdd'),
+      quantity: diff.toString()
+    })
+  })
+  console.log(`Today views: ${diff}`)
+  console.log(`Total views: ${count}`)
+  writeFileSync(PATH, count)
+}
+
+main()
