09328ea58c5ef77517d187af7bc9cd13a7a9e284
jigsaw <m@jgs.me>
2022-09-16 17:54:00 +0900

    Add lastmod

--- sitemap.js
+++ sitemap.js
@@ -1,17 +1,18 @@
-const { writeFileSync } = require('fs')
-const { Readable } = require('stream')
-const { SitemapStream, streamToPromise } = require('sitemap')
-const master = require('./data/pub.json')
+const { writeFileSync } = require("fs");
+const { Readable } = require("stream");
+const { SitemapStream, streamToPromise } = require("sitemap");
+const master = require("./data/pub.json");
 
-const links = master.map(page => ({
-  url: `/${page.id}.html`
-}))
+const links = master.map((page) => ({
+  url: `/${page.id}.html`,
+  lastmod: new Date(page.updated * 1000).toISOString(),
+}));
 const stream = new SitemapStream({
-  hostname: 'https://amp.kbys.tk',
-  cacheTime: 600000
-})
+  hostname: "https://amp.kbys.tk",
+  cacheTime: 600000,
+});
 
-;(async () => {
-  const xml = await streamToPromise(Readable.from(links).pipe(stream))
-  writeFileSync('./public/sitemap.xml', xml)
-})()
+(async () => {
+  const xml = await streamToPromise(Readable.from(links).pipe(stream));
+  writeFileSync("./public/sitemap.xml", xml);
+})();
